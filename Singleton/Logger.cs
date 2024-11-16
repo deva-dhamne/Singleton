a470ffaab3cc4b07395389920203dcd77fa85638
Devashish Dhamne <Devashish@villaplus.com>
2024-11-16 20:30:45 +0530

    Add project files.

---
+++ Singleton/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+
+        private static readonly object _lock = new object();
+
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null) 
+                    {
+                        _instance = new Logger();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var logger1 = Logger.GetInstance();
+            var logger2 = Logger.GetInstance();
+
+            logger1.Log("First message");  
+            logger2.Log("Second message"); 
+
+            Console.WriteLine(ReferenceEquals(logger1, logger2)); 
+        }
+    }
+}
